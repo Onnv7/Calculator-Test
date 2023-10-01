44aa8a8965177a358c7edf6f671298d16a78840b
Nguyễn Văn An <nva6112002@gmail.com>
2023-10-01 20:27:58 +0700

    Sửa lỗi tràn số

--- Form1.cs
+++ Form1.cs
@@ -31,6 +31,19 @@ namespace Buoi07_TinhToan3
             if (dr == DialogResult.Yes)
                 this.Close();
         }
+        private string ConvertToScientificNotation(double number)
+        {
+            if (number == 0)
+            {
+                return "0";
+            }
+            int exponent = (int)Math.Floor(Math.Log10(Math.Abs(number)));
+            double significand = number / Math.Pow(10, exponent);
+
+            string result = $"{Math.Round(significand, 1):0.#}*1E+{exponent}";
+
+            return result;
+        }
 
         private void btnTinh_Click(object sender, EventArgs e)
         {
@@ -44,7 +57,18 @@ namespace Buoi07_TinhToan3
             else if (radNhan.Checked) kq = so1 * so2;
             else if (radChia.Checked && so2 != 0) kq = so1 / so2;
             //Hiển thị kết quả lên trên ô kết quả
-            txtKq.Text = kq.ToString();
+            if(kq.ToString().Length > 10)
+			{
+                txtKq.Text = ConvertToScientificNotation(kq);
+            }
+            else
+            {
+                Console.WriteLine(kq.ToString());
+
+                txtKq.Text = kq.ToString();
+
+            }
+            
         }
     }
 }
